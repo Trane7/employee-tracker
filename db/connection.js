6a2e05eb0ce51543db03f8ac45ff8867ffd1f113
Dee <dgrowette@gmail.com>
2022-02-16 14:05:50 -0800

    added connection to server.js file

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
     host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    database: "nodejs_employee_tracker",
+    database: "employeesDB",
   },
   console.log("connected to the nodejs_employee_tracker database")
 )
