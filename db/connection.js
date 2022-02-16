578925184e1e064fc0a44689c733fe815338830e
Dee <dgrowette@gmail.com>
2022-02-16 15:03:20 -0800

    re-added connection.js file and filled it

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,14 @@
-// const mysql = require("mysql2")
+const mysql = require("mysql2")
 
-// const db = mysql.createConnection(
-//   {
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password7",
-//     database: "employees_db",
-//   },
-//   console.log("connected to the employees_db database")
-// )
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password7",
+    database: "employees_db",
+  },
+  console.log("connected to the employees_db database")
+)
 
-// module.exports = db
+module.exports = db
